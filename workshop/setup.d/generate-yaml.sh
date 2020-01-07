5fee3b51c4db0ee595a5c8a425b10a9da338ce83
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-01-07 15:20:46 +1100

    Incorrect backquote.

--- workshop/setup.d/generate-yaml.sh
+++ workshop/setup.d/generate-yaml.sh
@@ -6,7 +6,7 @@ cat exercises/image.yaml.in | envsubst > exercises/image.yaml
 
 # Fill in the IP address of the local image registry service.
 
-REGISTRY_IP=`kubectl get service/registry -o template --template '{{.spec.clusterIP}}'``
+REGISTRY_IP=`kubectl get service/registry -o template --template '{{.spec.clusterIP}}'`
 export REGISTRY_IP
 
 cat exercises/deployment.yaml.in | envsubst > exercises/deployment.yaml
